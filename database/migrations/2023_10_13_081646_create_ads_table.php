cc14a1e3c1c755ede1055638f0a28a530f9fa8b4
Lucas-Debiais <lucas.debiais11@gmail.com>
2023-10-13 10:30:19 +0200

    feat: add ads

---
+++ database/migrations/2023_10_13_081646_create_ads_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('category');
+            $table->text('description');
+            $table->decimal('price');
+            $table->string('city');
+            $table->string('state');
+            $table->bigInteger('year_prod');
+            $table->bigInteger('height');
+            $table->bigInteger('width');
+            $table->bigInteger('depth');
+            $table->bigInteger('weight');
+            $table->date('expiration_date');
+            $table->string('delivery');
+            $table->text('warranties');
+            $table->string('trade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ads');
+    }
+};
